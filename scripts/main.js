fb235142158eaa7135b52f3b53b38a9c4f07c586
Joshua Davidson <joshuadavidson@users.noreply.github.com>
2016-06-21 23:23:41 -0500

    Functioning calc. Need to refactor.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,14 @@
+function input(key) {
+
+}
+
 $(document).ready(function() {
+
+  var answer = '0'; //answer
+
+  $(document).keypress((event) => {
+
+  });
 
   //handle button presses
   $('button').click(function() {
@@ -14,22 +24,30 @@ $(document).ready(function() {
     } else if (newValue === 'AC') { //All Clear Pressed
       entryField.val('');
       resultField.val('0');
+      answer = 0;
     } else if (newValue === 'DEL') { //Delete Pressed
       entryField.val((index, currentVal) => {
-        return currentVal.slice(0, currentVal.length - 1);
+        if (currentVal.charAt(currentVal.length - 1) === 'S') { //account for the case that the last entry was ANS
+          return currentVal.slice(0, currentVal.length - 3); //remove the 3 characters of ANS
+        } else {
+          return currentVal.slice(0, currentVal.length - 1); //remove only the last character
+        }
       });
-    } else if (newValue === 'PLUS-MINUS') { //Plus/Minus Pressed
-
-    } else if (newValue === 'EQUALS') { //Equals Pressed
-      resultField.val(eval(entryField.val()).toString());
+    } else if (newValue === 'EQUALS' && /^[ANSE0-9./*\-+]*$/.test(entryField.val())) { //Equals Pressed AND Only evaluate if only calculator characters, prevent malicious code execution
+      answer = eval(entryField.val().replace(/ANS/g, answer)).toString(); //evaluate mathmatical string epxression using eval, replacing ANS with previous answer
+      entryField.val(''); //clear the entry field
+      resultField.val(answer); //display the answer
     } else {
       entryField.val((index, currentVal) => {
         if (/[/*\-+]$/.test(currentVal) && /[/*\-+]/.test(newValue)) { //If previous and current entry was operand then remove it and replace with new operand
           return currentVal.slice(0, currentVal.length - 1) + newValue;
-        } else {
+        } else if (currentVal === '' && /[/*\-+]/.test(newValue)) { //If blank and operand pressed perform operand on answer
+          return answer + newValue;
+        } else { //If number pressed just return it
           return currentVal + newValue;
         }
       });
     }
+
   });
 });
